97bfded4218c415b624b4690f9eea5f831386552
adessy <adrien.dessy@student.uclouvain.be>
2010-02-26 09:27:48 +0100

    Version eclipse

--- sms.py
+++ sms.py
@@ -14,9 +14,101 @@ def main():
 
 	for line in smslines:
 		line = line.upper()
-		line = line.replace('2',' TO ')
-		line = line.replace('4',' FOR ')
 		
+		line = line.replace(',',' <comma> ')
+		
+		# les balises ne doivent contenir que du texte pour findall
+		line = line.replace('</TEXT>',' <text_end> ')
+		line = line.replace('<TEXT>',' <text_start> ')
+		
+		line = line.replace('&APOS;',' <apos> ')
+		
+		# mots courant
+		reMC = r"(?<=\W)E(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' THE ',line,count=0)
+		
+		reMC = r"(?<=\W)(U|YA)(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' YOU ',line,count=0)
+		
+		reMC = r"(?<=\W)R(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' ARE ',line,count=0)
+		
+		reMC = r"(?<=\W)N(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' AND ',line,count=0)
+		
+		reMC = r"(?<=\W)C(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' SEE ',line,count=0)
+		
+		reMC = r"(?<=\W)(UR|YR)(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' YOUR ',line,count=0)
+		
+		reMC = r"(?<=\W)(I\WAM|IAM|IM)(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' I &APOS; M ',line,count=0)
+		
+		reMC = r"(?<=\W)WILL(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' &APOS;LL ',line,count=0)
+		
+		reMC = r"(?<=\W)(DUN|DUNNO)(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' DO NOT ',line,count=0)
+		
+		reMC = r"(?<=\W)NT(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' NOT ',line,count=0)
+		
+		reMC = r"(?<=\W)WAT(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' WHAT ',line,count=0)
+		
+		reMC = r"(?<=\W)DEN(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' THEN ',line,count=0)
+		
+		reMC = r"(?<=\W)(WAN|WANNA)(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' WANT ',line,count=0)
+		
+		
+		reMC = r"(?<=\W)(JUZ|JUS)(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' JUST ',line,count=0)
+		
+		reMC = r"(?<=\W)(OREDI|LIAO)(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' ALREADY ',line,count=0)
+
+		reMC = r"(?<=\W)S(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' IS ',line,count=0)
+		
+		reMC = r"(?<=\W)B(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' BE ',line,count=0)
+		
+		reMC = r"(?<=\W)TMR(?=\W)"
+		reMC = re.compile(reMC)
+		line = reMC.sub(' TOMORROW ',line,count=0)
+		
+		#TODO regarde s'il faut changer K (et OKAY) en OK
+		
+		reNum = r"(?<=[^1-9])2(?=[^1-9])"
+		reNum = re.compile(reNum)
+		line  = reNum.sub('2',line,count=0)
+		
+		reNum = r"(?<=[^1-9])4(?=[^1-9])"
+		reNum = re.compile(reNum)
+		line  = reNum.sub('4',line,count=0)
+		
+		line = line.replace('HAV ', 'HAVE ')
+		line = line.replace('&APOS;',' <apos> ')
 		
 		# match les sries de points (>1 point)
 		reDots = '\.\.+'
@@ -24,29 +116,31 @@ def main():
 		line = reDots.sub(' <dots> ',line,count=0)
 		
 		reDot = r"\.|!|\?"
-		reDot = re.compile
+		reDot = re.compile(reDot)
+		line = reDot.sub(' <dot> ',line,count=0)
 		
-		line = line.replace('.',' DOT ')
-		
-		reSmiley = '([;:][-o]?)|=[\(\)pP]'
+		reNum = r"\d+"
+		reNum = re.compile(reNum)
+		line  = reNum.sub(' <number> ',line,count=0)
+
+		reSmiley = '(([;:][-o]?)|=)[\(\)pP]'
 		reSmiley = re.compile(reSmiley)
-		line = reSmiley.sub(' SMILEY ',line,count=0)
+		line = reSmiley.sub(' <smiley> ',line,count=0)
 		
 		
-		#TODO rassembler les différents points.
 		
 		
-		line = line.replace('?',' INTERROGATIONDOT ')
-		line = line.replace('!',' EXCLAMATIONDOT ')
-		
-		line = line.replace('<TEXT>','')
-		line = line.replace('</TEXT>','')
+		#print line
+		#line = line.replace('<TEXT>','')
+		#line = line.replace('</TEXT>','')
 		#tokens = re.split(r"\W+|[.,!?;]",line)
-		tokens = re.split('\W+',line)
-		tokenline = ['S_BEGIN']
+		
+		reSplit = r"(<?\w+>?)"
+		reSplit = re.compile(reSplit)
+		tokens = reSplit.findall(line)
+		tokenline = []
+		print tokens
 		for token in tokens:
-			if token.isdigit():
-				token = 'NUMBER'
 			if token == '':
 				continue
 			tokenline.append(token)
@@ -54,7 +148,6 @@ def main():
 				lexicon[token] += 1
 			else:
 				lexicon[token] = 1
-		tokenline.append('S_END')
 		tokenlines.append(tokenline)
 
 	#sorting lexicon
@@ -69,6 +162,12 @@ def main():
 	
 	print lexicon_list
 
+	
+	
+	# Compute a dictionary of N-grams
+	#def ngramCount(tokenlines,order) :
+	#	for line in tokenlines : 
+	#		for i in range(len(line)-
 	
 	#ngramcon
 	for line in tokenlines:
@@ -116,8 +215,8 @@ def main():
 	#print 'yo'
 
 	
-	for x in histogram(unifreq):
-		print x
+	#for x in histogram(unifreq):
+	#	print x
 
 	
 	#print lexicon_list 
