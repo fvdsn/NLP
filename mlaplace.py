c9bdfe7387e7f579ec5332649997f3275a87aa26
adessy <adrien.dessy@student.uclouvain.be>
2010-03-01 11:26:44 +0100

    after w-e

---
+++ mlaplace.py
@@ -0,0 +1,105 @@
+'''
+Created on 26 fevr. 2010
+
+@author: Adrien
+'''
+
+import math
+
+class Model:
+    '''
+    classdocs
+    '''
+    
+    def __init__(self,training,order):
+        '''
+        Constructor
+        '''
+        self.order = order
+        self.training = training
+        
+        self.histoCount = self.__ngramCount(training,order-1)
+        self.ngCount = self.__ngramCount(training, order)
+        self.lexicon = self.__ngramCount(training, 1)
+        self.n = len(self.lexicon)
+        
+        print self.lexicon
+    
+    def shannon_game(self,histo):
+        propositions = []
+        for word in self.lexicon.keys() : 
+            if word != ():    
+                propositions.append((self.probNG(word[0],histo),word[0]))
+      
+        propositions.sort(reverse=True)
+        return propositions
+        
+    def probNG(self,word,histo):
+        sentence = histo + (word,)
+        cSentence = 0
+        cHisto = 0
+        
+        if histo    in self.histoCount.keys() : 
+            cHisto = self.histoCount[histo]
+            if sentence in self.ngCount.keys() : 
+                cSentence = self.ngCount[sentence]
+        
+        
+        return (cSentence+1)/(1.0 * cHisto + self.n) 
+    
+    def __ngramCount(self,tokenlines,order) :
+        print order
+        start = ('<text_start>',)
+        count = {}
+        for line in tokenlines:
+        
+            if order > 2 : 
+                for i in range(2,order):
+                    key = start*(order-i) + tuple(line[0:i])
+                    if key in count:
+                        count[key] += 1
+                    else :
+                        count[key]=1
+                    
+               
+            for i in range(len(line)-order+2):
+                key = tuple(line[i:i+order])
+                if key in count:
+                    count[key] += 1
+                else :
+                    count[key] = 1
+    
+        return count    
+    
+    def probText(self,testlines): 
+        logprob = 0
+        for line in testlines : 
+            logprob += self.probLine(line)
+        
+        return logprob 
+            
+            
+    def probLine(self,line):
+        line =  ['<text_start>']*(self.order-2) + line
+        logprob = 0
+        i = 0
+        for word in line[self.order-1:]:
+            histo = tuple(line[i:(i+self.order-1)])
+            #print "word "  + str(word)
+            #print "histo " + str(histo)
+            prob = self.probNG(word,histo)
+            #print "p:"+str(prob)
+            if prob>0 : 
+                logprob += math.log(prob,2) 
+            i+=1
+            
+        
+        return logprob
+            
+    def perplexity(self,testset):
+        
+        prob = self.probText(testset)
+        
+        return 2**(prob/self.n)
+         
+        
--- sms.py
+++ sms.py
@@ -1,10 +1,18 @@
+# TODO change the modification with WILL
+# insert an unknown word in the voc
+# check if k could be replaced by OK 
+# remove the start and end tags for the histogram computation
+# new smiley ^_^
+
 #!/usr/bin/python
 import sys
 import re
+import mlaplace
+import token
 
 def count(lexicons,ngram):
 	"""renvoie le nombre de fois que le ngram apparait dans le texte
-	   lexicons est un dico associant le degré n au lexicon des ngrammes de degré n"""
+	   lexicons est un dico associant le degre n au lexicon des ngrammes de degre n"""
 	n = len(ngram)
 	if n in lexicons.keys():
 		if ngram in lexicons[n]:
@@ -12,14 +20,14 @@ def count(lexicons,ngram):
 	return 0.0
 
 def maximium_likelihood(lexicons, ngram, prefix):
-	"""renvoie la probabilité P(w|h) avec ngram = h,w et prefix = h, lexicons: voir count"""
+	"""renvoie la probabilite P(w|h) avec ngram = h,w et prefix = h, lexicons: voir count"""
 	if count(prefix) > 0:
 		return count(lexicons,ngram)/count(lexicons,prefix)
 	else:
 		return 0.0
 
-def laplace(lexicons,ngram,prefix):
-	"""renvoie la probabilité P(w|h), avec laplace smoothing avec ngram = h,w et prefix = h, lexicons: voir count"""
+def laplacef(lexicons,ngram,prefix):
+	"""renvoie la probabilite P(w|h), avec laplace smoothing avec ngram = h,w et prefix = h, lexicons: voir count"""
 	wlen = len(ngram)-len(prefix)			# vaut normalement 1
 	w = ngram[len(ngram)-wlen:len(ngram)]	# prendre les wlen dernier tokens de ngram
 	Wcount = 0								# |W|
@@ -32,242 +40,77 @@ def KN(lexicons,ngram,prefix):
 	def gamma(lexicons,prefix,dc):
 		g = 0
 
-def main():
-	if len(sys.argv) < 2:
-		exit()
-	smsfile = open(sys.argv[len(sys.argv)-1])
-	smslines = smsfile.readlines()
-	lexicon = {}
-	tokenlines = []
-	twogramcon = {}
-	threegramcon = {}
 
-	for line in smslines:
-		line = line.upper()
 		
-		line = line.replace(',',' <comma> ')
-		
-		# les balises ne doivent contenir que du texte pour findall
-		line = line.replace('</TEXT>',' <text_end> ')
-		line = line.replace('<TEXT>',' <text_start> ')
-		
-		line = line.replace('&APOS;',' <apos> ')
-		
-		# mots courant
-		reMC = r"(?<=\W)E(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' THE ',line,count=0)
-		
-		reMC = r"(?<=\W)(U|YA)(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' YOU ',line,count=0)
-		
-		reMC = r"(?<=\W)R(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' ARE ',line,count=0)
-		
-		reMC = r"(?<=\W)N(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' AND ',line,count=0)
-		
-		reMC = r"(?<=\W)C(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' SEE ',line,count=0)
-		
-		reMC = r"(?<=\W)(UR|YR)(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' YOUR ',line,count=0)
-		
-		reMC = r"(?<=\W)(I\WAM|IAM|IM)(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' I &APOS; M ',line,count=0)
-		
-		reMC = r"(?<=\W)WILL(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' &APOS;LL ',line,count=0)
-		
-		reMC = r"(?<=\W)(DUN|DUNNO)(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' DO NOT ',line,count=0)
-		
-		reMC = r"(?<=\W)NT(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' NOT ',line,count=0)
-		
-		reMC = r"(?<=\W)WAT(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' WHAT ',line,count=0)
-		
-		reMC = r"(?<=\W)DEN(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' THEN ',line,count=0)
-		
-		reMC = r"(?<=\W)(WAN|WANNA)(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' WANT ',line,count=0)
-		
-		
-		reMC = r"(?<=\W)(JUZ|JUS)(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' JUST ',line,count=0)
-		
-		reMC = r"(?<=\W)(OREDI|LIAO)(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' ALREADY ',line,count=0)
 
-		reMC = r"(?<=\W)S(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' IS ',line,count=0)
+def ngramCount(tokenlines,order) :
+	start = ('<text_start>',)
+	count = {}
+	for line in tokenlines:
 		
-		reMC = r"(?<=\W)B(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' BE ',line,count=0)
-		
-		reMC = r"(?<=\W)TMR(?=\W)"
-		reMC = re.compile(reMC)
-		line = reMC.sub(' TOMORROW ',line,count=0)
-		
-		#TODO regarde s'il faut changer K (et OKAY) en OK
-		
-		reNum = r"(?<=[^1-9])2(?=[^1-9])"
-		reNum = re.compile(reNum)
-		line  = reNum.sub('2',line,count=0)
-		
-		reNum = r"(?<=[^1-9])4(?=[^1-9])"
-		reNum = re.compile(reNum)
-		line  = reNum.sub('4',line,count=0)
-		
-		line = line.replace('HAV ', 'HAVE ')
-		line = line.replace('&APOS;',' <apos> ')
-		
-		# match les sries de points (>1 point)
-		reDots = '\.\.+'
-		reDots = re.compile(reDots)
-		line = reDots.sub(' <dots> ',line,count=0)
-		
-		reDot = r"\.|!|\?"
-		reDot = re.compile(reDot)
-		line = reDot.sub(' <dot> ',line,count=0)
-		
-		reNum = r"\d+"
-		reNum = re.compile(reNum)
-		line  = reNum.sub(' <number> ',line,count=0)
-
-		reSmiley = '(([;:][-o]?)|=)[\(\)pP]'
-		reSmiley = re.compile(reSmiley)
-		line = reSmiley.sub(' <smiley> ',line,count=0)
-		
-		
-		
-		
-		#print line
-		#line = line.replace('<TEXT>','')
-		#line = line.replace('</TEXT>','')
-		#tokens = re.split(r"\W+|[.,!?;]",line)
-		
-		reSplit = r"(<?\w+>?)"
-		reSplit = re.compile(reSplit)
-		tokens = reSplit.findall(line)
-		tokenline = []
-		#print tokens
-		for token in tokens:
-			if token == '':
-				continue
-			tokenline.append(token)
-			if token in lexicon:
-				lexicon[token] += 1
-			else:
-				lexicon[token] = 1
-		tokenlines.append(tokenline)
-
-	#sorting lexicon
-	lexicon_list = []
-	for lex in lexicon.keys():
-		lexicon_list.append( (lex, lexicon[lex]) )
-	
-	def comp(a,b):
-		return  b[1] - a[1] 
-	
-	lexicon_list.sort(comp)
-	
-	print lexicon_list
-
-	
-	# Compute a dictionary of N-grams
-	def ngramCount(tokenlines,order) :
-		start = ('<text_start>',)
-		count = {}
-		for line in tokenlines:
-			
-			if order > 2 : 
-				for i in range(2,order):
-					key = start*(order-i) + tuple(line[0:i])
-					if key in count:
-						count[key] += 1
-					else :
-						count[key]=1
-						
-				
-			for i in range(len(line)-order+2):
-				key = tuple(line[i:i+order])
+		if order > 2 : 
+			for i in range(2,order):
+				key = start*(order-i) + tuple(line[0:i])
 				if key in count:
 					count[key] += 1
 				else :
-					count[key] = 1
-		
-		return count
+					count[key]=1
+					
+				
+		for i in range(len(line)-order+1):
+			key = tuple(line[i:i+order])
+			if key in count:
+				count[key] += 1
+			else :
+				count[key] = 1
+	
+	return count
 							
-#	#ngramcon
-#	for line in tokenlines:
-#		for i in range(len(line)-2):
-#			key = tuple(line[i:i+3])
-#			if key in threegramcon:
-#				threegramcon[key] +=1
-#			else:
-#				threegramcon[key] = 1
-#		for i in range(len(line)-1):
-#			key = tuple(line[i:i+2])
-#			if key in twogramcon:
-#				twogramcon[key] +=1
-#			else:
-#				twogramcon[key] = 1
-#	
-#	threegramcon_list = []
-#	for lex in threegramcon.keys():
-#		threegramcon_list.append( (lex, threegramcon[lex]) )
-#	threegramcon_list.sort(comp)
-#
-#	twogramcon_list = []
-#	for lex in twogramcon.keys():
-#		twogramcon_list.append( (lex, twogramcon[lex]) )
-#	twogramcon_list.sort(comp)
-#
-#	#print threegramcon_list
-#	threefreq = [ x[1] for x in threegramcon_list]
-#	twofreq = [ x[1] for x in twogramcon_list]
-#	unifreq = [ x[1] for x in lexicon_list]
-#
-#	def histogram(list):
-#		hist = {}
-#		for x in list:
-#			if x in hist:
-#				hist[x] += 1
-#			else:
-#				hist[x] = 1
-#		list = hist.values()
-#		list.sort()
-#		list.reverse()
-#		return list
+def histogram(ngramCount):
+	""" Retourne une liste de paires (nb x, nb de ngram apparaissant x fois)."""
+	hist = {}
+	for x in ngramCount.values():
+		if x in hist:
+			hist[x] += 1
+		else:
+			hist[x] = 1
+	pairs = hist.items()
+	pairs.sort(reverse=True)
+	return pairs
 
-	#print threefreq
-	#print 'yo'
-
+def maxLikely(ngramCount,histoCount,word,histo):
 	
-	#for x in histogram(unifreq):
-	#	print x
+	sentence = histo + (word,)
+	if sentence in ngramCount.keys():
+		return ngramCount[sentence]/(1.0 * histoCount[histo])
+	return 0.0
 
+def laplace(ngramCount,histoCount,word,histo,n):
+	sentence = histo + (word,)
+	if sentence in ngramCount.keys():
+		return (ngramCount[sentence]+1)/(1.0 * histoCount[histo] + n)
+	return 0.0
+
+def main():
+	if len(sys.argv) < 2:
+		exit()
 	
-	#print lexicon_list 
-	#print tokenlines
+	argsNb = len(sys.argv)
+	trainfile = open(sys.argv[argsNb-1])
+	testfile = open("../smstest.txt")
+	
+	traintokenlines = token.smartTokenizer(trainfile)
+	print traintokenlines
+	
+	mod = mlaplace.Model(traintokenlines,3)
+	
+	#print mod.perplexity(token.smartTokenizer(testfile))
+	print mod.shannon_game(("BOTH","OF"))
+	
+	#print mod.probText(tokenizer(testfile))
+	#str = ["<text_start>","OKKO","<dotsi>","YESLO","<dot>","<text_end>"]
+	
+	
+	
 if __name__ == '__main__':
 	main()
